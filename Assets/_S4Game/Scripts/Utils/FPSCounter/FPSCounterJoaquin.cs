93fa4288c0d857b1928b79f67169f5d9e104ee5b
JoaquinBuzzles96 <joakilm2@gmail.com>
2020-12-28 12:55:48 +0100

    Initial commit

---
+++ Assets/_S4Game/Scripts/Utils/FPSCounter/FPSCounterJoaquin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounterJoaquin : MonoBehaviour
+{
+    [SerializeField]
+    private Text FramesText;
+
+    float refrehRate = 0.5f;
+    float timer = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            timer = refrehRate;
+            FramesText.text = $"FPS: {1/Time.deltaTime}";
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
