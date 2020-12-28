46a634b63994d0489db166ded8341990fcdd69b6
JoaquinBuzzles96 <joakilm2@gmail.com>
2020-12-28 14:47:23 +0100

    Textures and first panel version added

---
+++ Assets/Scripts/Dialogue/OptionsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsManager : MonoBehaviour
+{
+
+    public List<TextMeshProUGUI> options;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
