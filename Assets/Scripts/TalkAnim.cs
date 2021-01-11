df2454f00ccbb18ae4a421faf530076ea64c55f6
Sergio1811 <serroddev@gmail.com>
2021-01-11 15:06:41 +0100

    Titulos con animacion de color y escena 7 como en el esquema

---
+++ Assets/Scripts/TalkAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TalkAnim : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float speed;
+    public void Start()
+    {
+        //StartCoroutine(ToGreen());
+    }
+
+    public void Update()
+    {
+       text.color =  Color.Lerp(Color.white, Color.green, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+}
+
