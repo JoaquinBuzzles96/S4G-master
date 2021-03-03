db11391c3b87cea1d483b54e61a4f845ce75e889
Sergio1811 <serroddev@gmail.com>
2021-03-03 10:06:18 +0100

    Lanzar objeto

---
+++ Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowObject : MonoBehaviour
+{
+    public GameObject objectToThrow;
+    public GameObject objectiveToThrow;
+    Rigidbody rb;
+    Vector3 direction = Vector3.forward;
+    public int force;
+
+    private void Start()
+    {
+        rb = objectToThrow.GetComponent<Rigidbody>();
+        direction = (objectiveToThrow.transform.position - this.transform.position).normalized;
+    }
+    public void ReleaseObject()
+    {
+        objectToThrow.transform.parent = null;
+
+        rb.useGravity = true;
+        rb.AddForce(direction * force);
+    }
+}
