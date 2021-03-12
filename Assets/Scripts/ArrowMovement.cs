71e85b3bef8d48f66ef72bdd1fa40502fcb73f35
JoaquinBuzzles96 <joakilm2@gmail.com>
2021-03-12 09:29:12 +0100

    Arrow reception

---
+++ Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public float maxMovement = 2f;
+    public float speed = 3f;
+    float originalY;
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, originalY + (Mathf.PingPong(speed, maxMovement)), transform.position.z);
+        //Mathf.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+}
