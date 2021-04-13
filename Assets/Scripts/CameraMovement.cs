c5fbd22c2f7f3a90990c5b1c7cda20b6a30812c7
Sergio1811 <48327928+Sergio1811@users.noreply.github.com>
2021-04-13 10:27:44 +0200

    Camaras en todas las escenas con ratón

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public float speed = 25.0f;
+	public Vector3 initialPos;
+
+
+	private void Start()
+    {
+		//this.transform.position = initialPos;
+    }
+    void Update()
+	{
+		if (Input.GetMouseButton(1))
+		{
+			if (Input.GetAxis("Mouse X") != 0)
+			{
+				transform.eulerAngles += new Vector3(Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse X") * Time.deltaTime * -speed, 0.0f);
+			}
+		}
+	}
+}
