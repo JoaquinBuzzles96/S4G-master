38d2091805c80c540052b251b6bde2d9391ac92c
Sergio1811 <48327928+Sergio1811@users.noreply.github.com>
2021-04-23 08:38:57 +0200

    desktop cam
    se me olvidó subirlo la verdad

---
+++ Assets/Scripts/ActivateCamVR.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCamVR : MonoBehaviour
+{
+    public GameObject vr;
+    public GameObject cameraNotVR;
+    private void Awake()
+    {
+#if UNITY_EDITOR_WIN || UNITY_EDITOR 
+        cameraNotVR.SetActive(true);
+        vr.SetActive(false);
+#endif
+
+    }
+}
